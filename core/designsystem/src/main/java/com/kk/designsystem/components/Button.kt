ea274ba1d9c8de07e030543532945fbf23451035
Diego Garcia <gciadiego@diegos-mbp.lan>
2023-02-01 17:09:13 -0500

    Design components

---
+++ core/designsystem/src/main/java/com/kk/designsystem/components/Button.kt
@@ -0,0 +1,54 @@
+package com.kk.designsystem.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.kk.designsystem.theme.BurntSienna
+import com.kk.designsystem.theme.KkTypography
+import com.kk.designsystem.theme.RedSalsa
+import com.kk.designsystem.theme.ShamrockGreen
+import java.util.*
+
+@Composable
+fun KkButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    label: String,
+    border: BorderStroke = BorderStroke(2.dp, BurntSienna),
+    colors: ButtonColors = ButtonDefaults.outlinedButtonColors(contentColor = BurntSienna)
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        border = border,
+        colors = colors,
+        shape = RoundedCornerShape(6.dp)
+    ) {
+        Text(text = label.uppercase(Locale.ROOT), style = KkTypography.titleSmall)
+    }
+}
+
+@Composable
+fun KkCorrectButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    label: String
+) {
+    KkButton(onClick = { onClick }, label = label, border = BorderStroke(2.dp, ShamrockGreen), colors = ButtonDefaults.outlinedButtonColors(contentColor = ShamrockGreen))
+}
+
+@Composable
+fun KkIncorrectButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    label: String
+) {
+    KkButton(onClick = { onClick }, label = label, border = BorderStroke(2.dp, RedSalsa), colors = ButtonDefaults.outlinedButtonColors(contentColor = RedSalsa))
+}
---
+++ core/designsystem/src/main/java/com/kk/designsystem/components/Text.kt
@@ -0,0 +1,50 @@
+package com.kk.designsystem.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.kk.designsystem.theme.BurntSienna
+import com.kk.designsystem.theme.KkTypography
+import com.kk.designsystem.theme.RedSalsa
+import com.kk.designsystem.theme.ShamrockGreen
+import java.util.*
+
+@Composable
+fun KkTitle(
+    modifier: Modifier = Modifier,
+    label: String
+){
+    Text(text = label, style = KkTypography.titleMedium, modifier = modifier)
+}
+
+@Composable
+fun KkBody(
+    modifier: Modifier = Modifier,
+    label: String
+){
+    Text(text = label, style = KkTypography.bodyLarge, modifier = modifier)
+}
+
+@Composable
+fun KkOrangeTitle(
+    modifier: Modifier = Modifier,
+    label: String
+){
+    Text(text = label.uppercase(Locale.ROOT), color = BurntSienna, style = KkTypography.titleLarge, modifier = modifier)
+}
+
+@Composable
+fun KkCorrectTitle(
+    modifier: Modifier = Modifier,
+    label: String
+){
+    Text(text = label.uppercase(Locale.ROOT), color = ShamrockGreen, style = KkTypography.titleLarge, modifier = modifier)
+}
+
+@Composable
+fun KkIncorrectTitle(
+    modifier: Modifier = Modifier,
+    label: String
+){
+    Text(text = label.uppercase(Locale.ROOT), color = RedSalsa, style = KkTypography.titleLarge, modifier = modifier)
+}
