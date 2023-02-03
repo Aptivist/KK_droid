a9bc56c78d9477b5759a6eed48bb84873d63c6b7
Lupillo17 <joseguadaluperivera17@gmail.com>
2023-02-03 14:02:34 -0500

    Fix vertical scrolling when screen change to Land Scape

--- presentation/src/main/java/com/kk/presentation/ScreenCreateRoom.kt
+++ presentation/src/main/java/com/kk/presentation/ScreenCreateRoom.kt
@@ -1,6 +1,8 @@
 package com.kk.presentation
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,6 +22,7 @@ fun ScreenCreateRoom() {
 
     Column(
         modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxSize()
             .padding(horizontal = 30.dp)
     ) {
