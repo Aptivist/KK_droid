bfb0d95bc760e8eaa1020e947ba5d927e40cd916
juanislas97 <58410224+juanislas77@users.noreply.github.com>
2023-02-06 17:30:16 -0500

    Add RateAnswerAdminView

--- presentation/src/main/java/com/kk/presentation/ProgressGameView.kt
+++ presentation/src/main/java/com/kk/presentation/ProgressGameView.kt
@@ -1,15 +1,13 @@
 package com.kk.presentation
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.kk.designsystem.components.KkBody
-import com.kk.designsystem.components.KkButton
-import com.kk.designsystem.components.KkOrangeTitle
-import com.kk.designsystem.components.KkTitle
+import com.kk.designsystem.components.*
 
 @Composable
 fun PreStartAdminView(round: String){
@@ -64,5 +62,43 @@ fun ShowWinnerAdminView(round: String, winnerName: String){
             KkButton(onClick = { }, label = stringResource(id = R.string.next_button), modifier = Modifier.fillMaxWidth())
         }
 
+    }
+}
+
+@Composable
+fun RateAnswerAdminView( round: String, playerAnswer: String, showSkip:Boolean){
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(30.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            KkTitle(label = round)
+        }
+        Box(modifier = Modifier.padding(25.dp, 0.dp)) {
+            KkBody(label = stringResource(id = R.string.player_answer))
+        }
+        Box(modifier = Modifier.padding(25.dp)) {
+            KkBody(label = playerAnswer)
+        }
+        Row(modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp, 30.dp), horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.Bottom) {
+            AnimatedVisibility(visible = showSkip) {
+                KkButton(onClick = { }, label = stringResource(id = R.string.skip))
+            }
+            AnimatedVisibility(visible = !showSkip) {
+                KkIncorrectButton(
+                    onClick = { },
+                    label = stringResource(id = R.string.incorrect_answer_button)
+                )
+
+            }
+            AnimatedVisibility(visible = !showSkip) {
+                KkCorrectButton(
+                    onClick = { },
+                    label = stringResource(id = R.string.correct_answer_button)
+                )
+            }
+        }
     }
 }
