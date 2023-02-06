f3a39c20be0230738dec0b0066e30ef8f9aee378
juanislas97 <58410224+juanislas77@users.noreply.github.com>
2023-02-06 13:40:44 -0500

    Optimize imports

--- presentation/src/main/java/com/kk/presentation/ProgressGameView.kt
+++ presentation/src/main/java/com/kk/presentation/ProgressGameView.kt
@@ -1,13 +1,10 @@
 package com.kk.presentation
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kk.designsystem.components.KkBody
 import com.kk.designsystem.components.KkButton
