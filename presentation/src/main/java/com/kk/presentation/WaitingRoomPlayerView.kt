904ed02b8622063e8b3baaee8ece683fa2fa2d29
Jesus Lopez <jesus.l.g.dmn@gmail.com>
2023-02-06 17:44:13 -0500

    WaitingRoomPlayerView made, also added strings files

--- presentation/src/main/java/com/kk/presentation/WaitingRoomPlayerView.kt
+++ presentation/src/main/java/com/kk/presentation/WaitingRoomPlayerView.kt
@@ -1,11 +1,13 @@
 package com.kk.presentation
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.kk.designsystem.components.KkBody
 import com.kk.designsystem.components.KkChip
 import com.kk.designsystem.components.KkTitle
