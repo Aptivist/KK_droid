6c2865e946312953a9915a28a680e69159aa9a83
gciadiego <gciadiegoa@gmail.com>
2023-02-08 14:39:42 -0500

    Added the Ktor Client

--- core/network/src/main/java/com/kk/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/kk/network/di/NetworkModule.kt
@@ -2,15 +2,21 @@ package com.kk.network.di
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.logging.*
-import io.ktor.client.plugins.websocket.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import io.ktor.client.features.websocket.*
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
 
-/*val networkModuleKtor = module {
+val networkModuleKtor = module {
     singleOf(::provideHttpClient)
-}*/
+}
 
 fun provideHttpClient() = HttpClient(CIO){
     install(Logging)
     install(WebSockets)
-    //install(JsonFeature)
+    install(JsonFeature){
+        serializer = KotlinxSerializer()
+    }
 }
