a730b25ddbb23c977f236241cec32072d47f59e3
Jesus Lopez <51238232+NomasChuy@users.noreply.github.com>
2023-02-10 12:46:06 -0500

    Created the domain models, the mappers and imported the domain module into data module gradle (#41)

---
+++ core/domain/src/main/java/com/kk/domain/models/PlayerUserDomain.kt
@@ -0,0 +1,8 @@
+package com.kk.domain.models
+
+data class PlayerUserDomain(
+    val id: String,
+    val name: String,
+    var points: Int? = null,
+    override val code: String,
+): UserDomain()
---
+++ core/domain/src/main/java/com/kk/domain/models/UserDomain.kt
@@ -0,0 +1,5 @@
+package com.kk.domain.models
+
+abstract class UserDomain{
+    abstract val code: String?
+}
