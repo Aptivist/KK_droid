993619c8d776b3a56766b2ad924a7e513288e0b8
Diego Beltrán Maldonado <diego.beltranm.95@outlook.com>
2023-02-13 15:45:56 -0500

    Onclick was added in Text components

--- core/designsystem/src/main/java/com/kk/designsystem/components/Text.kt
+++ core/designsystem/src/main/java/com/kk/designsystem/components/Text.kt
@@ -1,5 +1,6 @@
 package com.kk.designsystem.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,54 +11,65 @@ import java.util.*
 @Composable
 fun KkTitle(
     modifier: Modifier = Modifier,
-    label: String
+    label: String,
+    onClick: (() -> Unit)? = null
 ) {
-    Text(text = label, style = KkTypography.titleMedium, modifier = modifier)
+    Text(
+        text = label,
+        style = KkTypography.titleMedium,
+        modifier = modifier.clickable { onClick?.invoke() })
 }
 
 @Composable
 fun KkBody(
     modifier: Modifier = Modifier,
-    label: String
+    label: String,
+    onClick: (() -> Unit)? = null
 ) {
-    Text(text = label, style = KkTypography.bodyLarge, modifier = modifier)
+    Text(
+        text = label,
+        style = KkTypography.bodyLarge,
+        modifier = modifier.clickable { onClick?.invoke() })
 }
 
 @Composable
 fun KkOrangeTitle(
     modifier: Modifier = Modifier,
-    label: String
+    label: String,
+    onClick: (() -> Unit)? = null
 ) {
     Text(
         text = label.uppercase(Locale.ROOT),
         color = MaterialTheme.colorScheme.onPrimary,
         style = KkTypography.titleLarge,
-        modifier = modifier
+        modifier = modifier.clickable { onClick?.invoke() }
     )
 }
 
 @Composable
 fun KkCorrectTitle(
     modifier: Modifier = Modifier,
-    label: String
+    label: String,
+    onClick: (() -> Unit)? = null
 ) {
     Text(
         text = label.uppercase(Locale.ROOT),
         color = MaterialTheme.colorScheme.tertiary,
         style = KkTypography.titleLarge,
-        modifier = modifier
+        modifier = modifier.clickable { onClick?.invoke() }
     )
 }
 
 @Composable
 fun KkIncorrectTitle(
     modifier: Modifier = Modifier,
-    label: String
+    label: String,
+    onClick: (() -> Unit)? = null
 ) {
     Text(
         text = label.uppercase(Locale.ROOT),
         color = MaterialTheme.colorScheme.error,
         style = KkTypography.titleLarge,
-        modifier = modifier
+        modifier = modifier.clickable { onClick?.invoke() }
     )
 }
