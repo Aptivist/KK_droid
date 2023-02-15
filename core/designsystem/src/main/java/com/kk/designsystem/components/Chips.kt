44deba33cc6ed4b20cd4c11a69923e623e2db78f
gciadiego <gciadiegoa@gmail.com>
2023-02-15 16:26:27 -0500

    Modified the Chip component and gave an example on how to implement it in the 'WaitingRoomAdmin' View

--- core/designsystem/src/main/java/com/kk/designsystem/components/Chips.kt
+++ core/designsystem/src/main/java/com/kk/designsystem/components/Chips.kt
@@ -3,11 +3,13 @@ package com.kk.designsystem.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
@@ -20,11 +22,18 @@ fun KkChip(
 ){
     Box(
         modifier = modifier
-            .border(2.dp, MaterialTheme.colorScheme.onPrimary, RoundedCornerShape(20.dp))
-            .clip(RoundedCornerShape(20.dp))
-            .background(MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.3f))
-            .padding(15.dp, 5.dp, 15.dp, 5.dp)
+            .padding(end = 5.dp, bottom = 5.dp)
     ){
-        Text(text = label, style = KkTypography.titleSmall)
+        Box(
+            modifier = modifier
+                .border(1.dp, MaterialTheme.colorScheme.onPrimary, RoundedCornerShape(10.dp))
+                .clip(RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.3f))
+                .padding(10.dp,5.dp,10.dp,5.dp)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Text(text = label, style = KkTypography.titleSmall)
+        }
     }
 }
