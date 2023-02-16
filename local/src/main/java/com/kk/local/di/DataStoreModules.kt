8023351da69ee740b363b75693de2d4e1b83e015
Yaritza Moreno <yaritza.moreno94@outlook.com>
2023-02-16 14:49:09 -0600

    Datastore modules with Koin

---
+++ local/src/main/java/com/kk/local/di/DataStoreModules.kt
@@ -0,0 +1,10 @@
+package com.kk.local.di
+
+import com.kk.local.data.PreferencesRepositoryImpl
+import com.kk.local.domain.PreferencesRepository
+import org.koin.dsl.module
+
+val dataStoreModule = module {
+    single<PreferencesRepository> {
+        PreferencesRepositoryImpl(get()) }
+}
