ae46df5bee046eb16725fa6b2aa62006e95ec8c6
Diego Beltrán Maldonado <diego.beltranm.95@outlook.com>
2023-02-16 17:15:05 -0500

    conflicts were resolved

--- core/data/src/main/java/com/kk/data/repository/WaitingRoomAdminRepository.kt
+++ core/data/src/main/java/com/kk/data/repository/WaitingRoomAdminRepository.kt
@@ -4,5 +4,6 @@ import com.kk.domain.models.*
 import kotlinx.coroutines.flow.Flow
 
 interface WaitingRoomAdminRepository {
+    suspend fun sendRequest(eventRequestDomain: EventRequestDomain)
     fun receivePlayers() : Flow<BaseResult<PlayersResponse>>
 }
