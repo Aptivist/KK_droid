b41bd5686d854279da4074e97e8d00d41ecd09d4
Jesus Lopez <jesus.l.g.dmn@gmail.com>
2023-02-17 15:13:33 -0500

    Creating files that I need

--- core/data/src/main/java/com/kk/data/repository/UserAnswerRepositoryImp.kt
+++ core/data/src/main/java/com/kk/data/repository/UserAnswerRepositoryImp.kt
@@ -1,8 +1,25 @@
 package com.kk.data.repository
 
 import com.google.gson.Gson
+import com.kk.domain.models.AnswerDomain
+import com.kk.domain.models.BaseResponseDomain
+import com.kk.domain.models.BaseResult
 import com.kk.network.service.ISocketService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
-class UserAnswerRepositoryImp(private val socketServie: ISocketService, private val gson: Gson) :
+class UserAnswerRepositoryImp(private val socketService: ISocketService, private val gson: Gson) :
     UserAnswerRepository {
+    override suspend fun sendAnswer(requestAnswer: AnswerDomain) {
+        TODO("Not yet implemented")
+    }
+
+    override fun receiveData(): Flow<BaseResult<BaseResponseDomain<AnswerDomain>>> {
+        return try {
+            socketService.receiveData().map { BaseResult.Success(gson.fromJson(it)) }
+        } catch (ex: Exception){
+            flow { emit(BaseResult.Error(ex)) }
+        }
+    }
 }
