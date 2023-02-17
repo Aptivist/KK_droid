534f174d3d63ff7a6f080528c5a16fca43274d50
Daniel Roldan <dmroldanchavez@gmail.com>
2023-02-16 19:35:21 -0500

    Join Room Implementation

--- presentation/src/main/java/com/kk/presentation/player/joinroom/JoinRoomView.kt
+++ presentation/src/main/java/com/kk/presentation/player/joinroom/JoinRoomView.kt
@@ -8,71 +8,81 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.kk.designsystem.components.KkBody
-import com.kk.designsystem.components.KkButton
-import com.kk.designsystem.components.KkTextField
-import com.kk.designsystem.components.KkTitle
+import com.kk.designsystem.components.*
 import com.kk.presentation.R
+import com.kk.presentation.host.creategame.CreateRoomContract
+import com.kk.presentation.host.creategame.CreateRoomViewModel
+import kotlinx.coroutines.flow.collectLatest
+import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
 fun JoinRoomView(
     onBackHome : () -> Unit,
-    navigateToWaitingRoom : () -> Unit
+    navigateToWaitingRoom : () -> Unit,
+    viewModel: JoinRoomViewModel = koinViewModel()
 ) {
-    var nameValue by remember { mutableStateOf("") }
-    var codeValue by remember { mutableStateOf("") }
 
-    Column(
-        modifier = Modifier
-            .verticalScroll(rememberScrollState())
-            .fillMaxSize()
-            .padding(horizontal = 30.dp)
-    ) {
-        KkTitle(
-            label = stringResource(R.string.jg_join_room_title),
+    val uiState by viewModel.uiState.collectAsState()
+    KKBox() {
+        Column(
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(vertical = 40.dp)
-        )
-        KkBody(label = stringResource(R.string.jr_name))
-        Spacer(modifier = Modifier.size(10.dp))
-        KkTextField(
-            value = nameValue,
-            onValueChange = {
-                nameValue = it
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-        )
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .padding(horizontal = 30.dp)
+        ) {
+            KkTitle(
+                label = stringResource(R.string.jg_join_room_title),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(vertical = 40.dp)
+            )
+            KkBody(label = stringResource(R.string.jr_name))
+            Spacer(modifier = Modifier.size(10.dp))
+            KkTextField(
+                value = uiState.name,
+                onValueChange = {
+                    viewModel.handleEvent(JoinRoomContract.Event.OnChangeName(it))
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
 
-        Spacer(modifier = Modifier.size(20.dp))
-        KkBody(label = stringResource(R.string.jr_code))
-        Spacer(modifier = Modifier.size(10.dp))
-        KkTextField(
-            value = codeValue,
-            onValueChange = {
-                codeValue = it.uppercase()
-            },
-            modifier = Modifier
-                .fillMaxWidth()
-        )
+            Spacer(modifier = Modifier.size(20.dp))
+            KkBody(label = stringResource(R.string.jr_code))
+            Spacer(modifier = Modifier.size(10.dp))
+            KkTextField(
+                value = uiState.code,
+                onValueChange = {
+                    viewModel.handleEvent(JoinRoomContract.Event.OnChangeCode(it))
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
 
-        KkButton(
-            onClick = { /*TODO*/ },
-            label = stringResource(R.string.jr_scan_code),
-            modifier = Modifier
-                .padding(vertical = 40.dp)
-                .fillMaxWidth()
-        )
+            KkButton(
+                onClick = { /*TODO*/ },
+                label = stringResource(R.string.jr_scan_code),
+                modifier = Modifier
+                    .padding(vertical = 40.dp)
+                    .fillMaxWidth()
+            )
 
-        Spacer(modifier = Modifier.weight(1f))
-        KkButton(
-            onClick = { navigateToWaitingRoom.invoke() },
-            label = stringResource(R.string.jr_join_room),
-            modifier = Modifier
-                .padding(vertical = 50.dp)
-                .fillMaxWidth()
-        )
+            Spacer(modifier = Modifier.weight(1f))
+            KkButton(
+                onClick = { viewModel.handleEvent(JoinRoomContract.Event.OnJoinButtonClicked) },
+                label = stringResource(R.string.jr_join_room),
+                modifier = Modifier
+                    .padding(vertical = 50.dp)
+                    .fillMaxWidth()
+            )
+        }
+    }
+    LaunchedEffect(Unit){
+        viewModel.effect.collectLatest {
+            when(it){
+                JoinRoomContract.Effect.Navigate -> navigateToWaitingRoom()
+            }
+        }
     }
 }
