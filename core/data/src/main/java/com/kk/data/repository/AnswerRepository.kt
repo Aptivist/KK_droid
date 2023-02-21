8ab5452c5e284f43a71d1e8eaebecf32272122ab
Diego Beltrán Maldonado <diego.beltranm.95@outlook.com>
2023-02-21 16:51:51 -0600

    repository answer was finished

--- core/data/src/main/java/com/kk/data/repository/AnswerRepository.kt
+++ core/data/src/main/java/com/kk/data/repository/AnswerRepository.kt
@@ -2,13 +2,11 @@ package com.kk.data.repository
 
 import com.kk.domain.models.BaseResponseDomain
 import com.kk.domain.models.BaseResult
+import com.kk.domain.models.EventAnswerRequestDomain
 import com.kk.domain.models.KKTimerDomain
 import kotlinx.coroutines.flow.Flow
 
 interface AnswerRepository {
-
-    suspend fun sendAnswer()
-
+    suspend fun sendAnswer(eventAnswerRequestDomain: EventAnswerRequestDomain)
     fun receiveData() : Flow<BaseResult<BaseResponseDomain<KKTimerDomain>>>
-
 }
--- core/domain/src/main/java/com/kk/domain/models/EventAnswerRequestDomain.kt
+++ core/domain/src/main/java/com/kk/domain/models/EventAnswerRequestDomain.kt
@@ -2,5 +2,5 @@ package com.kk.domain.models
 
 data class EventAnswerRequestDomain(
     val event : String = "SEND_ANSWER",
-    val answerDomain: PlayersAnswerDomain
+    val answerDomain: AnswerDomain
 )
